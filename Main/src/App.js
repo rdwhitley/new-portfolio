e99e188f4e822384258c3a278914c8899fd98942
Rafiq <raw96@icloud.com>
2021-08-05 23:27:00 -0400

    css loaders added App component created

---
+++ Main/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './main.css';
+
+class App extends React.Component {
+    render(){
+        return (
+            <div className='container'>
+                <div>1</div>
+                <div>2</div>
+                <div>3</div>
+            </div>
+        )
+    }
+}
+
+export default App;
